5eefd9eed15c130d4337116bfcdbc8e9cc441bc3
safaanilatasoy <safaanilatasoy@gmail.com>
2022-03-23 15:44:33 +0300

    main

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Profile from './Components/Home/Profile';
+import Footer from './Components/Home/Footer/Footer'
 
 function App() {
   return ( 
   <div className="App">
     <Profile/>
+    <Footer/>
+    
   </div>
   );
 }
